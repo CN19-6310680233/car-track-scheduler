379d87cc2d514fa96bd32653afebd42ac21e1142
Aom Siriwat <siriwat576@gmail.com>
2023-03-23 22:15:26 +0700

    fix: bring back file extenstion validator

--- src/video/video.controller.ts
+++ src/video/video.controller.ts
@@ -20,7 +20,7 @@ export class VideoController {
     async uploadVideo(
         @Body()
         data: VideoDTO,
-        @UploadedFile()
+        @UploadedFile(new ParseFilePipeBuilder().addFileTypeValidator({fileType: /(avi|mov|wmv|mp4)/}).build())
         file: Express.Multer.File
     ) {
         const fileExt = path.extname(file.originalname);
