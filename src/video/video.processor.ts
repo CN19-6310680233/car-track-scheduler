8528a10b9588dea4e391142e84074473817866c8
Aom Siriwat <siriwat576@gmail.com>
2023-03-23 22:15:08 +0700

    feat: processor webhook

--- src/video/video.processor.ts
+++ src/video/video.processor.ts
@@ -1,4 +1,5 @@
 import { Processor, Process } from '@nestjs/bull';
+import axios from 'axios';
 import { Job } from 'bull';
 import { VIDEO_QUEUE } from 'src/config/queue';
 
@@ -11,11 +12,26 @@ export class VideoProcessor {
 
     // Process the video file
     for(let i = 0; i < 100; i ++) {
-      await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 1000)));
+      await new Promise((resolve) => setTimeout(resolve, Math.floor(Math.random() * 500)));
       await job.progress(i)
     }
     await job.progress(100);
-    // ...
-    return {status: true};
+
+    const webhookParams = {
+      job_id: job.id,
+      result: {
+        frame: Math.floor(Math.random() * 200), 
+        count: Math.floor(Math.random() * 100),
+        file_name: filePath,
+      }
+    };
+    const webhook = await axios.post(process.env.WEBHOOK_API_ENDPOINT, webhookParams).then(data => {
+      return data.data;
+    }).catch(err => null);
+    
+    return {
+      status: true,
+      webhook
+    };
   }
 }
